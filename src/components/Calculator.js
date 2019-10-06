27d28d88098cd56d76e16341207009f5739e9014
Arthur E. Jones <PartyLich@gmail.com>
2019-10-06 01:08:53 -0400

    feat: Calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { pipe } from '../lib/lib';
+
+import { CalcButton, CalcDisplay } from '../components';
+
+const log = (val) => {
+  console.log(val);
+  return val;
+};
+
+/**
+ * Calculator component
+ * @param {object} props
+ * @return {ReactElement}
+ */
+export const Calculator = (props) => {
+  // event handlers
+  const { equalClick, clearAll, operandClick, operatorClick } = props;
+
+  const makeButton = (modifier = 'square') => (clickHandler = log) => (
+      value
+  ) => (
+    <CalcButton
+      value={value}
+      modifier={modifier}
+      onClick={clickHandler}
+    />
+  );
+
+  const operandButton = (modifier = 'square') =>
+    makeButton(modifier)(
+        pipe(
+            log,
+            operandClick(props)
+        )
+    );
+
+  const operatorButton = (modifier = 'square') =>
+    makeButton(modifier)(
+        pipe(
+            log,
+            operatorClick(props)
+        )
+    );
+
+  const handleKeyPress = (event) => {
+    const numbers = new Set([
+      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']);
+    const operators = new Set(['+', '-', '/', '*']);
+    const numeric = numbers.has(event.key);
+    const operator = operators.has(event.key);
+    const enter = event.key === 'Enter';
+
+    if (numeric) operandClick(props)(event.key);
+    if (operator) operatorClick(props)(event.key);
+    if (enter) equalClick(props)();
+  };
+
+  const numpad = [
+    ['7', '8', '9'],
+    ['4', '5', '6'],
+    ['1', '2', '3'],
+  ];
+
+  return (
+    <div
+      class="calc"
+      onKeyDown={handleKeyPress}
+      tabIndex="0"
+    >
+      <CalcDisplay {...props.display} />
+      <div class="calc-keypad">
+        {makeButton('wide')(pipe(
+            log,
+            clearAll
+        ))('AC')}
+        {operatorButton()('/')}
+        {numpad.map((row) => row.map(operandButton()))}
+        {operandButton('wide')('0')}
+        {operandButton()('.')}
+      </div>
+      <div class="calc-op-pad">
+        {['*', '-', '+'].map(operatorButton())}
+        {makeButton('tall')(pipe(
+            log,
+            equalClick(props)
+        ))('=')}
+      </div>
+    </div>
+  );
+};
+
+Calculator.propTypes = {
+  isOperator: PropTypes.bool,
+  operators: PropTypes.arrayOf(PropTypes.string),
+  operands: PropTypes.arrayOf(PropTypes.string),
+  display: PropTypes.shape({
+    input: PropTypes.string,
+    output: PropTypes.string,
+  }),
+
+  clearAll: PropTypes.func,
+  equalClick: PropTypes.func,
+  operandClick: PropTypes.func,
+  operatorClick: PropTypes.func,
+};
